bb57f2d9b3aae4c624b52926df4f27af886b6524
Nya <sntdawuk@qq.com>
2025-02-12 18:17:44 +0800

    [Feature] nothing

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+/*
+ [Feature]
+ [Bug]
+ [Patch]
+ [Release]
+ [Clean]
+ */
+
 struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
