c17569f3ec088285819856a8c34e9abc4b4d42ea
Nya <sntdawuk@qq.com>
2025-02-12 17:48:25 +0800

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by nya on 2025/2/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
